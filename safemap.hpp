8b629ddee8c9da4d8111509db674f195e930ce06
Simon Julian Lauw <simonjulian21@gmail.com>
2023-02-19 23:38:45 +0800

    Add get or default

--- safemap.hpp
+++ safemap.hpp
@@ -6,44 +6,54 @@
 #include <optional>
 #include <shared_mutex>
 
-template <typename Key, typename Val>
+template<typename Key, typename Val>
 class SafeMap {
 private:
-  std::shared_mutex mtx;
-  std::unordered_map<Key, Val> hmap;
+    std::shared_mutex mtx;
+    std::unordered_map<Key, Val> hmap;
 
 public:
-  void put(const std::pair<Key, Val>& item) {
-    std::unique_lock lock(mtx);
-    // if (hmap.find(item.first) != hmap.end())
-    //   hmap.erase(item.first);
-    hmap.insert(item);
-  }
-  
-  bool contains(const Key& key) {
-    std::shared_lock lock(mtx);
-    bool exists = (hmap.find(key) != hmap.end());
-    return exists;
-  }
+    void put(const std::pair<Key, Val> &item) {
+        std::unique_lock lock(mtx);
+        // if (hmap.find(item.first) != hmap.end())
+        //   hmap.erase(item.first);
+        hmap.insert(item);
+    }
 
-  std::optional<Val&> get(const Key& key) {
-    std::shared_lock lock(mtx);
-    auto ptr = hmap.find(key);
-    std::optional<std::reference_wrapper<Val>> ret = (ptr != hmap.end()) ? ptr->second : std::nullopt;
-    return ret;
-  }
-  
-  void erase(const Key& key) {
-    std::unique_lock lock(mtx);
-    if (hmap.find(key) == hmap.end())
-      return;
-    hmap.erase(key);
-  }
-  
-  uint32_t size() {
-    std::shared_lock lock(mtx);
-    return hmap.size();
-  }
+    bool contains(const Key &key) {
+        std::shared_lock lock(mtx);
+        bool exists = (hmap.find(key) != hmap.end());
+        return exists;
+    }
+
+    Val &getOrDefault(const Key &key, const Val &default_value) {
+        typename std::unordered_map<Key, Val>::iterator ptr;
+        {
+            std::shared_lock lock(mtx);
+            ptr = hmap.find(key);
+        }
+        if (ptr == hmap.end()) {
+            {
+                std::unique_lock lock(mtx);
+                hmap.insert(default_value);
+                return hmap.find(key)->second;
+            }
+        }
+
+        return ptr->second;
+    }
+
+    void erase(const Key &key) {
+        std::unique_lock lock(mtx);
+        if (hmap.find(key) == hmap.end())
+            return;
+        hmap.erase(key);
+    }
+
+    uint32_t size() {
+        std::shared_lock lock(mtx);
+        return hmap.size();
+    }
 
 };
 
